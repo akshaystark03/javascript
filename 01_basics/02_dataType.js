5f9014ecd27c19c433ee75cc07bc3c0872496178
akshaystark03 <akshay@starkedge.com>
2026-02-27 09:39:02 +0000

    data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,8 @@
+"use strict"; // new version code k liye ye line likhte h
+
+
+// console.log(3 + 
+    
+//     3); // ye code likhne ka shi trika nhi h // this code not right way
+
+console.log('Hello Akshay'); // ye shi trika h code likhne ka // this is rith way 
