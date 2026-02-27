86185a2a0bbd7b825e63ae6d196f4780b30c3ae3
akshaystark03 <akshay@starkedge.com>
2026-02-27 10:00:11 +0000

    data type

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -6,3 +6,18 @@
 //     3); // ye code likhne ka shi trika nhi h // this code not right way
 
 console.log('Hello Akshay'); // ye shi trika h code likhne ka // this is rith way 
+
+
+let name = "Akshay";
+let age = 18;
+let isLogedIn = true;
+
+
+// data type 
+
+// 1 string = "";
+// 2 number = 2 to 53;
+// 3 bigint = 53 se uper;
+// 4 Boolean = true/flase;
+// 5 Null = value nhi hai;
+// 6 undefind = value difined nhi ki 
